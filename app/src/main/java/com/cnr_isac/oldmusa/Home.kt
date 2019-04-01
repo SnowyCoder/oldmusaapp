3d990076aa0ce558f66c5d40cc013c40ddc5bf8c
Giorgia Bertacchini <giorgia.bertacchini92@gmail.com>
2019-04-01 09:02:38 +0200

    added new layouts

--- app/src/main/java/com/cnr_isac/oldmusa/Home.kt
+++ app/src/main/java/com/cnr_isac/oldmusa/Home.kt
@@ -1,10 +1,20 @@
 package com.cnr_isac.oldmusa
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.add_museum.view.*
+import android.view.ViewGroup
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.add_museum.*
+
 
 class Home : AppCompatActivity() {
 
@@ -23,6 +33,26 @@ class Home : AppCompatActivity() {
         mDrawerlayout.addDrawerListener(mToggle)
         mToggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        addSiti.setOnClickListener{
+            //val mDialogView = LayoutInflater.from(this).inflate(R.layout.add_museum, null)
+
+            val mBuilder = AlertDialog.Builder(this)
+            mBuilder.setTitle("Aggiungi museo")
+            val d = mBuilder.setView(LayoutInflater.from(this).inflate(R.layout.add_museum, null)).create()
+            val lp = WindowManager.LayoutParams()
+            lp.copyFrom(d.window!!.attributes)
+            lp.title = "Aggiungi museo"
+            lp.width = (resources.displayMetrics.widthPixels * 0.80).toInt()
+            lp.height = (resources.displayMetrics.heightPixels * 0.50).toInt()
+            d.show()
+            d.window!!.attributes = lp
+
+            d.AddButtonM.setOnClickListener {
+                d.dismiss()
+
+            }
+        }
 
     }
 
--- app/src/main/java/com/cnr_isac/oldmusa/Museum.kt
+++ app/src/main/java/com/cnr_isac/oldmusa/Museum.kt
@@ -1,9 +1,14 @@
 package com.cnr_isac.oldmusa
 
+import android.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.CheckBox
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_museum.*
+import kotlinx.android.synthetic.main.add_museum.view.*
+import kotlinx.android.synthetic.main.add_sensor.view.*
 
 class Museum : AppCompatActivity() {
 
@@ -13,6 +18,40 @@ class Museum : AppCompatActivity() {
 
         addMapbutton.setOnClickListener {
             val mDialogView = LayoutInflater.from(this).inflate(R.layout.add_map, null)
+
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Aggiungi un sensore")
+
+            val mAlertDialog = mBuilder.show()
+
+            mDialogView.AddButtonM.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+        }
+
+        addSensorbutton.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.add_sensor, null)
+
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Aggiungi un sensore")
+
+            val mAlertDialog = mBuilder.show()
+
+            mDialogView.AddButtonS.setOnClickListener {
+                mAlertDialog.dismiss()
+                //val name = mDialogView.nameMuseum.text.toString()
+                //val channels: List<Int>
+
+            }
+
+            mDialogView.CloseS.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+
         }
     }
+
+
 }
