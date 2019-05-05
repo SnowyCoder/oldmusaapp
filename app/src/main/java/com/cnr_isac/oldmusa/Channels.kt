e6c2b4b4706adc560cf4e5a98cff2c7ddbcf22ef
Giorgia Bertacchini <giorgia.bertacchini92@gmail.com>
2019-05-05 19:02:47 +0200

    menu sensor

--- app/src/main/java/com/cnr_isac/oldmusa/Channels.kt
+++ app/src/main/java/com/cnr_isac/oldmusa/Channels.kt
@@ -1,9 +1,8 @@
 package com.cnr_isac.oldmusa
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageButton
 import android.widget.ListView
 import androidx.fragment.app.Fragment
@@ -15,6 +14,7 @@ class Channels : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+        setHasOptionsMenu(true)
 
         val view = inflater.inflate(R.layout.fragment_sensor, container, false)
 
@@ -31,4 +31,38 @@ class Channels : Fragment(){
 
         return view
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.remove -> {
+                val mBuilder = AlertDialog.Builder(context!!)
+                val dialog = mBuilder.setView(LayoutInflater.from(context!!).inflate(R.layout.remove_sensor, null)).create()
+                val lp = WindowManager.LayoutParams()
+                lp.copyFrom(dialog.window!!.attributes)
+                lp.width = (resources.displayMetrics.widthPixels * 0.75).toInt()
+                lp.height = (resources.displayMetrics.heightPixels * 0.30).toInt()
+                dialog.show()
+                dialog.window!!.attributes = lp
+            }
+            R.id.edit -> {
+                val mBuilder = AlertDialog.Builder(context!!)
+                mBuilder.setTitle("Modifica il sensore")
+                val dialog = mBuilder.setView(LayoutInflater.from(context!!).inflate(R.layout.edit_sensor, null)).create()
+                val lp = WindowManager.LayoutParams()
+                lp.copyFrom(dialog.window!!.attributes)
+                lp.title = "modifica il sensore"
+                lp.width = (resources.displayMetrics.widthPixels * 0.80).toInt()
+                lp.height = (resources.displayMetrics.heightPixels * 0.50).toInt()
+                dialog.show()
+                dialog.window!!.attributes = lp
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
