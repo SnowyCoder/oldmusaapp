db2685c7593b54c3de5b8575df38a3212e42fb74
Giorgia Bertacchini <giorgia.bertacchini92@gmail.com>
2019-05-08 15:24:27 +0200

    nothing

--- app/src/main/java/com/cnr_isac/oldmusa/Channels.kt
+++ app/src/main/java/com/cnr_isac/oldmusa/Channels.kt
@@ -3,14 +3,24 @@ package com.cnr_isac.oldmusa
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.AdapterView
 import android.widget.ImageButton
 import android.widget.ListView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.cnr_isac.oldmusa.api.Channel
 import com.cnr_isac.oldmusa.util.ApiUtil.isAdmin
 
 class Channels : Fragment(){
 
     private lateinit var listView: ListView
+
+    data class ChannelData(val handle: Channel) {
+        override fun toString(): String {
+            return handle.name ?: handle.id.toString()
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
@@ -25,7 +35,7 @@ class Channels : Fragment(){
             buttonVisible1.visibility=View.VISIBLE
         }*/
 
-        //listView = view.findViewById(R.id.channelList)
+        listView = view.findViewById(R.id.channelsList)
 
         view.findViewById<ImageButton>(R.id.addChannelButton).setOnClickListener{
             //val mDialogView = LayoutInflater.from(this).inflate(R.layout.add_museum, null)
@@ -43,7 +53,17 @@ class Channels : Fragment(){
             dialog.window!!.attributes = lp
         }
 
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, view, index, _ ->
+            val channel = listView.adapter.getItem(index) as ChannelData
+
+            //view.findNavController().navigate(ChannelsDirections.actionChannelToQuickGraph(channel.handle.id))
+        }
+
         return view
+    }
+
+    fun onChannelSelect(channelId: Long) {
+        view!!.findNavController().navigate(SiteDirections.actionSiteToChannel(channelId))
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
