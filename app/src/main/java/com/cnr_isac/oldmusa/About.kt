c6e3a4556e37340c3df62aa5e38c739a7c1514d9
Giorgia Bertacchini <giorgia.bertacchini92@gmail.com>
2019-05-16 17:17:38 +0200

    GitHub link

--- app/src/main/java/com/cnr_isac/oldmusa/About.kt
+++ app/src/main/java/com/cnr_isac/oldmusa/About.kt
@@ -1,10 +1,17 @@
 package com.cnr_isac.oldmusa
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_about.*
+import android.content.Intent
+import android.net.Uri
+import android.widget.Button
+import android.widget.TextView
+
 
 class About : Fragment() {
 
@@ -14,7 +21,17 @@ class About : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_about, container, false)
-
+        view.findViewById<TextView>(R.id.linkGitHub).setOnClickListener{
+            goToUrl()
+        }
         return view
     }
+
+    private fun goToUrl() {
+        val uriUrl = Uri.parse("https://github.com/OldMusa-5H/")
+        val launchBrowser = Intent(Intent.ACTION_VIEW, uriUrl)
+        startActivity(launchBrowser)
+    }
+
 }
+
